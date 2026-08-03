e3be29947c6f7d3984d17eacca9fab982cc7a1d5
yuvraj singh <yuvraj.s@mandasadevelopment.com>
2026-08-03 14:27:41 +0530

    change import

--- assistant/urls.py
+++ assistant/urls.py
@@ -1,11 +1,21 @@
+import os
+from django.conf import settings
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots_txt'),
+    path(
+        "robots.txt",
+        serve,
+        {
+            "document_root": os.path.join(settings.BASE_DIR, "assistant", "static"),
+            "path": "robots.txt",
+        },
+    ),
     path('google2f83435a64a9c20c.html', views.google_verification, name='google_verification'),
     path('api/command/', views.process_command_api, name='process_command_api'),
 ]
+
 
